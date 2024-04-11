b623c1c62c71b767d1f1cd5a16de9ad4de89facf
Rachael Bull <rachaelbull2021@gmail.com>
2024-04-11 14:19:45 +0000

    Create the post model

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Post # R imports the post model created
 
 # Register your models here.
+admin.site.register(Post) # R gives admin access to posts from the panel

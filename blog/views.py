63d3dcc8cf27c837881aaf631e15af8bb7af2b3f
Rachael Bull <rachaelbull2021@gmail.com>
2024-04-11 17:19:40 +0000

    Create generic homepage views

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views import generic
+from .models import Post
 
-# Create your views here.
-def the_blog(request):
-    return HttpResponse("This is my blog test message")
+class PostEntries(generic.ListView):
+    model = Post
+    queryset = Post.objects.all()
+    template_name = "post_list.html"

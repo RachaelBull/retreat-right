03502900a628cb9665a3b187bce262f5356ba510
Rachael Bull <rachaelbull2021@gmail.com>
2024-04-11 15:54:25 +0000

    Register Comment model in admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Post # R imports the post model created
+from .models import Post, Comment # R imports the post model created
 
 # Register your models here.
 admin.site.register(Post) # R gives admin access to posts from the panel
+admin.site.register(Comment)

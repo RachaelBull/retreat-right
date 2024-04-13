f3dbcaa8a4dea741fe1f7d1387f9b6563be93e2c
Rachael Bull <rachaelbull2021@gmail.com>
2024-04-13 13:36:14 +0000

    Ready for deployment

--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Post
 
+# Create your views here.
+
+
 class PostEntries(generic.ListView):
-    model = Post
     queryset = Post.objects.filter(status=1)
     template_name = "blog/index.html"
     paginate_by = 6

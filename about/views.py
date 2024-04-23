412ab8ca5b555bc620962bbc1b4188e9e4bc74be
Rachael Bull <rachaelbull2021@gmail.com>
2024-04-23 14:50:11 +0000

    Add functionality to the contact form

--- about/views.py
+++ about/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
+from django.contrib import messages
 from .models import About
 from .forms import ContactForm
 # Create your views here.
 
 
 def about_me(request):
+
+    if request.method == "POST":
+        contact_form = ContactForm(data=request.POST)
+        if contact_form.is_valid():
+            contact_form.save()
+            messages.add_message(request, messages.SUCCESS, "Contact request received! Please allow a few days for me to get back to you.")
     """
     Renders the About page
     """

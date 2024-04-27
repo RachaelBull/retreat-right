202b441d0cc2ff340b4ddb4c1d7373889af71923
Rachael Bull <rachaelbull2021@gmail.com>
2024-04-27 14:55:24 +0000

    Add profile nav link for authenticated users

--- userprofile/admin.py
+++ userprofile/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import userspage
 
 # Register your models here.
+admin.site.register(userspage)
--- userprofile/models.py
+++ userprofile/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class userspage(models.Model):
+    nickname = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200, null=True)
+    bio = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True, null=True)

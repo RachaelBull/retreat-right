2d8b3061fc07c96f0efc2fcaacc9fa45bcca188d
Rachael Bull <rachaelbull2021@gmail.com>
2024-05-06 16:17:39 +0000

    Create view for the user profile form

--- userprofile/models.py
+++ userprofile/models.py
@@ -3,10 +3,10 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class UsersPage(models.Model):
-    username = models.CharField(max_length=50, unique=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
     name = models.CharField(max_length=50)
     email = models.EmailField(max_length=250, default="")
-    bio = models.TextField()
+    bio = models.TextField(blank=True)
 
     def __str__(self):
         return f'{self.name}'
--- userprofile/views.py
+++ userprofile/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views import generic, View
-from .models import UsersPage
+from .models import UsersPage, User
 from .forms import UsersPageForm
 from django.http import HttpResponseRedirect
 from django.contrib import messages
@@ -9,11 +9,35 @@ from django.contrib import messages
 class UserProfile(View):
 
     def get(self, request):
-        user_profile_form = UsersPageForm
-        userprofile = UsersPage.objects.all()
-        return render(request,
-            "userprofile/userprofile.html",
+       if request.user.is_authenticated:
+            # if the users profile already exists
+            if UsersPage.objects.filter(user=request.user).exists():
+                user_profile_instance = UsersPage.objects.get(user=request.user)
+                user_profile_form = UsersPageForm(instance=user_profile_instance)
+            else:
+                user_profile_form = UsersPageForm()
+
+            return render(request, "userprofile/userprofile.html",
             {
-            "userprofile": userprofile,
-            'user_profile_form': user_profile_form
+                'user_profile_form': user_profile_form
             })
+
+    def post(self,request):
+        user_profile_instance = None
+        if UsersPage.objects.filter(user=request.user).exists():
+            user_profile_instance = UsersPage.objects.get(user=request.user)
+
+        user_profile_form = UsersPageForm(request.POST, instance=user_profile_instance)
+        if user_profile_form.is_valid():
+            user_profile = user_profile_form.save(commit=False)
+            user_profile.user = request.user
+            user_profile.save()
+
+            if user_profile_instance:
+                messages.success(request, "Your profile has been updated.")
+            else:
+                messages.success(request, "Your profile has been created.")
+
+            return redirect('home')
+
+        return render(request, "userprofile/userprofile.html", {'user_profile_form: user_profile_form'})               
